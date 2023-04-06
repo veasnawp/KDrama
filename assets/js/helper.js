daa7a453136d81d2ae2e4ba6d62cd97a69f6c871
Veasna Kheng <114059731+veasnawp@users.noreply.github.com>
2023-04-07 03:15:20 +0700

    Fetch JSON URL

---
+++ assets/js/helper.js
@@ -0,0 +1,18 @@
+// sample fetch json url function
+function getPlaylist(url, key, postName) {
+
+  if (url) url = url
+  else url = `https://rawcdn.githack.com/veasnawp/KDrama/${key}/archive/datas/${postName}`
+  
+  fetch(url, {
+      method: 'GET',
+      headers: {
+          'Accept': 'application/json',
+      },
+  }).then(response => response.json())
+    .then(response => {
+      data = response
+      //console.log(data)
+      return data
+    })
+}
